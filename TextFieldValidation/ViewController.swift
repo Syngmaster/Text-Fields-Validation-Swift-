fc96c2a719530914b742672a4f40f0476b8e0de8
Maksim Artemov <maksimartemov@maksims-macbook-pro.local>
2018-04-05 09:51:06 +0100

    decimal validation and at-sign validation added

--- TextFieldValidation/ViewController.swift
+++ TextFieldValidation/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +22,45 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension UIViewController: UITextFieldDelegate {
+    
+    
+    public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        return validationAtSignCheckFrom(textField: textField, string: string)
+    }
+    
+    func validationNumericDecimalCheckFrom(textField: UITextField, string: String) -> Bool {
+        
+        let period = textField.text?.filter{".".contains($0)}
+        if period == "." && string == "." {
+            return false
+        }
+        
+        var set = CharacterSet.decimalDigits
+        set.insert(charactersIn: ".")
+        let words = string.components(separatedBy: set.inverted)
+        if words.count > 1 {
+            return false
+        }
+        
+        return true
+    }
+    
+    func validationAtSignCheckFrom(textField: UITextField, string: String) -> Bool {
+        
+        if textField.text?.count == 0 && string == "@" {
+            return false
+        }
+        
+        let period = textField.text?.filter{"@".contains($0)}
+        if period == "@" && string == "@" {
+            return false
+        }
+        
+        return true
+    }
+    
 }
 
