88f157b448ef53323668f2d8e81e052d1da86d52
Maksim Artemov <maksimartemov@maksims-macbook-pro.local>
2018-04-05 09:41:25 +0100

    Initial Commit

---
+++ TextFieldValidation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextFieldValidation
+//
+//  Created by Maksim Artemov on 05/04/2018.
+//  Copyright © 2018 Maksim Artemov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
